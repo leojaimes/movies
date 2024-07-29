7dd2a7413832df663ff7c6188f5f48aa114b935d
leo.jaimes <leo777jaimes@gmail.com>
2024-07-29 17:51:22 -0600

    upgrade

--- app/models/movies_models.py
+++ app/models/movies_models.py
@@ -1,10 +1,14 @@
-
 from pydantic import BaseModel
+
 
 class CategoryBase(BaseModel):
     name: str
 
-class MovieBase(BaseModel):
-    name: str
-    category: CategoryBase
 
+class MovieBase(BaseModel):
+    id: int
+    title: str
+    overview: str
+    year: str
+    rating: float
+    category: CategoryBase
--- app/routers/movies_router.py
+++ app/routers/movies_router.py
@@ -1,15 +1,19 @@
 from typing import List
-from fastapi  import APIRouter
+from fastapi import APIRouter
 from app.models.movies_models import MovieBase, CategoryBase
+
 router = APIRouter()
 
-@router.get("/movies", tags=["Movies"], response_model=List[MovieBase] )
+
+@router.get("/movies", tags=["Movies"], response_model=List[MovieBase])
 def getMovies():
     return [
         MovieBase(
-            category = CategoryBase(
-                name= "Category 1"
-            ),
-            name="El retorno de la IA "
+            id=1,
+            title="El retorno de la IA ",
+            overview="En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+            year="2009",
+            rating=7.8,
+            category=CategoryBase(name="Action"),
         )
     ]
