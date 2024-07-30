5655e71803f6fc456feb5666abed8ab119d8e955
leo.jaimes <leo777jaimes@gmail.com>
2024-07-29 18:12:04 -0600

    funcion modificadorea

--- app/dependences/utils.py
+++ app/dependences/utils.py
@@ -1,20 +1,34 @@
-
-from  app.models.user_models  import UserBase
+from app.models.user_models import UserBase
 from typing import Generator
 from datetime import datetime
+import time
+
 
 def fibonnaci(n):
     a, b = 0, 1
     for _ in range(n):
         yield a
-        a,b = b, a + b
-
+        a, b = b, a + b
 
 
 def userData(n) -> Generator[UserBase, None, None]:
     for i in range(n):
-        yield  UserBase(
-            name=f"user {i}",
-            email=f"user{i}@gmail.com",
-            dateOfBirth= datetime.now()
+        yield UserBase(
+            name=f"user {i}", email=f"user{i}@gmail.com", dateOfBirth=datetime.now()
         )
+
+
+def calculateTime(func):
+    def modifiedFunction(n):
+        initTime = time.time()
+        func(n)
+        endTime = time.time()
+        print(f"Execution Time { endTime - initTime} seconds")
+
+    return modifiedFunction
+
+
+@calculateTime
+def imprimirNumeros(n):
+    for i in range(n):
+        print(i)
