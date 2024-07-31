88b451d9ca22ff4ae986958067c73cf8cb01e32e
leo.jaimes <leo777jaimes@gmail.com>
2024-07-31 09:50:42 -0600

    upgrade

--- app/dependences/pandas_ex.py
+++ app/dependences/pandas_ex.py
@@ -1,15 +1,38 @@
 import pandas as pd
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-dataFrame = pd.read_csv("meaningful_output.csv", index_col="id")
 
-# df_filtred = df.dropna() Delete rows with NaN values in any cell
-# df_filtred.head()
+# load enviroment variables from .env file
+load_dotenv()
 
-dataFrame.fillna(
-    value=0
-)  # It is necessary to receive a value; otherwise, an error will be shown.
-print(dataFrame)
+# read MONGO_DB_URI environment var
+mongo_uri = os.getenv("MONGO_DB_URI")
 
-dataFrame.fillna(
-    {"retweets": 0, "followers": 0}
-)  # "This instruction changes NaN values in the column 'retweets' to zero, and zeros in the column 'followers
+
+client = MongoClient(mongo_uri)
+
+db = client["teslo"]
+collection = db["products"]
+data = list(collection.find())
+
+
+df = pd.DataFrame(data)
+
+print(df)
+
+
+# dataFrame = pd.read_csv("meaningful_output.csv", index_col="id")
+
+# # df_filtred = df.dropna() Delete rows with NaN values in any cell
+# # df_filtred.head()
+
+# dataFrame.fillna(
+#     value=0
+# )  # It is necessary to receive a value; otherwise, an error will be shown.
+# print(dataFrame)
+
+# dataFrame.fillna(
+#     {"retweets": 0, "followers": 0}
+# )  # "This instruction changes NaN values in the column 'retweets' to zero, and zeros in the column 'followers
